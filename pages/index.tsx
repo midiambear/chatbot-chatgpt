ccb4e38d91870c8bde6c118204908f9a95998fe0
midiambear <midiambear8739@gmail.com>
2023-06-15 18:20:00 +0900

    My Chatbot

---
+++ pages/index.tsx
@@ -0,0 +1,73 @@
+/**
+ * ChatGPT API（gpt-3.5-turbo）を Node.js から使ってみる（公式リファレンスで参照した情報も記載）
+ * https://qiita.com/youtoy/items/b3e2e462029bf40f8a1a
+ * 　このコードをベースに。
+ * $ npm i openai
+ *
+ * https://platform.openai.com/docs/api-reference/chat/create?lang=node.js
+ *  これ、Example requestの右上にcurl, python, node.jsの選択がある！これは気が付かんわー
+ *
+ * https://openai.com/product
+ *
+ * https://platform.openai.com/account/api-keys
+ *
+ * https://twitter.com/yanosen_jp/status/1631074488363384832
+ *
+ * https://qiita.com/youtoy/items/84384ad7a742ea1ce8f5
+ *
+ * https://platform.openai.com/docs/libraries/node-js-library
+ *
+ * https://platform.openai.com/docs/api-reference/authentication
+ *
+ * https://platform.openai.com/docs/api-reference/searches
+ */
+
+ import React, { useState } from 'react';
+ import Button from "../components/button";
+ import TextBox from "../components/textbox";
+ import axios from "axios";
+ 
+ const ChatGptApiTest = () => {
+  const [answer, setAnswer] = useState('');
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
+   const callAI = async () => {
+     const chat: string = (
+       document!.getElementById("text-textbox") as HTMLInputElement
+     ).value;
+ 
+     const res = await axios.get("/api/chatgpt?chat=" + chat);
+     const data = await res.data;
+     console.log(data);
+     console.log(data.chat);
+     alert(data.chat);
+   };
+ 
+   return (
+     <div className="min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+       <div className="w-full max-w-md space-y-8  rounded-xl bg-gray-100 mx-auto my-2">
+         <div className="">
+           <div>
+             <TextBox
+               id="text-textbox"
+               name="text-textbox"
+               placeholder="何か質問を入力"
+             ></TextBox>
+           </div>
+           <Button
+             type="button"
+             onClick={() => {
+               callAI();
+             }}
+           >
+             ChatGPTへ送る
+           </Button>
+         </div>
+       </div>
+     </div>
+   );
+ };
+ 
+ export default ChatGptApiTest;
